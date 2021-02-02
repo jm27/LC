912c065fbccaa5a3fa603faf5973f6b0bb33ad03
Jesus Esquer <jme2791@gmail.com>
2021-02-02 09:48:51 -0700

    To lower case solution

---
+++ dayThirteen/to-lower-case.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function (str) {
+  // use toLowerCase() method
+  return str.toLowerCase();
+};
+
+/**
+ * Runtime: 76 ms, faster than 69.59% of JavaScript online submissions for To Lower Case.
+ * Memory Usage: 38.3 MB, less than 57.40% of JavaScript online submissions for To Lower Case.
+ */
