a65e3e2bde406acb598e5f4278ca7cd380c8baee
Jesus Esquer <jme2791@gmail.com>
2021-06-23 20:00:11 -0700

    Symbols javascript

---
+++ dayTwentyNine/private-object.js
@@ -0,0 +1,12 @@
+// Create two symbols - are useful for creating private variables.
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+    [username]: "Jesus Esquer",
+    [password]: "12345jme",
+    age:27
+};
+
+console.log(user.username);
+console.log(user.password)
