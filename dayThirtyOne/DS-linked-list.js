2178323071ed5824be507ed54b79401e0b3eddb9
Jesus Esquer <jme2791@gmail.com>
2021-06-29 19:53:55 -0700

    Linked list reverse method

--- dayThirtyOne/DS-linked-list.js
+++ dayThirtyOne/DS-linked-list.js
@@ -89,26 +89,64 @@ class SinglyLinkedList {
   }
   // Insert into a given index
   insert(index, val) {
-      if(index < 0 || index > this.length) return false;
-      if(index === this.length) return !!this.push(val);
-      if(index === 0) return !!this.unshift(val);
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
 
-      var newNode = new Node(val);
-      var prev = this.get(index - 1);
-      var temp = prev.next;
-      prev.next = newNode;
-      newNode.next = temp;
-      this.length++;
-      return true;
+    var newNode = new Node(val);
+    var prev = this.get(index - 1);
+    var temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
+  // Remove element from given index
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    var previousNode = this.get(index - 1);
+    var removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+  // Print the list in order
+  print() {
+    var arr = [];
+    var current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
+  reverse() {
+    var node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    var next;
+    var prev = null;
+    for (var i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
   }
 }
 
 let list = new SinglyLinkedList();
-list.push("Jesus");
-list.push("hello");
-list.push("Jesus");
-list.push("there");
-list.push("Jesus");
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+list.push(5);
 // console.log(list.set(4, "hola"));
-console.log(list.insert(3, 5));
-console.log(list.get(3));
+// console.log(list);
+list.print();
+// console.log(list);
+list.reverse();
+list.print();
