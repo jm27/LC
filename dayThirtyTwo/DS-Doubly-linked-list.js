43f499993497fdd326aba3ce563d73210141df30
Jesus Esquer <jme2791@gmail.com>
2021-06-30 13:00:37 -0700

    DS - Doubly linked list push and pop methods javascript

---
+++ dayThirtyTwo/DS-Doubly-linked-list.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  // Push to end of list
+  push(val) {
+    var newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  // Remove from the end
+  pop() {
+    if (this.length === 0) return undefined;
+    var poppedNode = this.tail;
+    if (this.length === 1) {
+      this.tail = null;
+      this.head = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+}
+
+const list = new DoublyLinkedList();
+list.push(11);
+console.log(list);
+list.push(112);
+list.pop();
+// console.log(list);
+// list.pop();
+console.log(list);
