fbf1df491eb77955a1e20cb32d10c9415ecec7b7
Jesus Esquer <jme2791@gmail.com>
2021-07-05 09:46:21 -0700

    Doubly linked list, shift, unshift, get, insert, set, methods

--- dayThirtyTwo/DS-Doubly-linked-list.js
+++ dayThirtyTwo/DS-Doubly-linked-list.js
@@ -41,13 +41,91 @@ class DoublyLinkedList {
     this.length--;
     return poppedNode;
   }
+  // Remove from beginning
+  shift() {
+    if (this.length === 0) return undefined;
+    var oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+  // Insert at the beginning.
+  unshift(value) {
+    let newNode = new Node(value);
+    if (this.length === 0) {
+      this.tail = newNode;
+      this.head = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  // Get element from a given position
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let current;
+    if (index <= this.length / 2) {
+      let count = 0;
+      current = this.head;
+      while (count != index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      let count = this.length - 1;
+      current = this.head;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+    }
+    return current;
+  }
+  // Update value from index
+  set(index, val) {
+    var foundNode = this.get(index);
+    if (foundNode !== null) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+  // Insert node on a given index.
+  insert(index, val) {
+    if (index < 0 || index > this.lenght) return false;
+    if (index === 0) return !!this.unshift(val);
+    if (index === this.length) return !!this.push(val);
+    var newNode = new Node(val);
+    var beforeNode = this.get(index - 1);
+    var afterNode = beforeNode.next;
+
+    (beforeNode.next = newNode), (newNode.prev = beforeNode);
+    (newNode.next = afterNode), (afterNode.prev = newNode);
+    this.length++;
+    return true;
+  }
 }
 
 const list = new DoublyLinkedList();
 list.push(11);
-console.log(list);
+// console.log(list);
 list.push(112);
-list.pop();
+// list.pop();
 // console.log(list);
 // list.pop();
+// list.shift();
+// list.shift()
+// console.log(list);
+// list.unshift("HEllo");
+console.log(list.insert(1, "hola"));
 console.log(list);
