9fedbc73f0d8f002f968790aa056f7537ae5c020
Jesus Esquer <jme2791@gmail.com>
2021-07-07 11:51:08 -0700

    create a stack with javascript

---
+++ dayThirtyThree/stacks.js
@@ -0,0 +1,14 @@
+/**
+ * Create a Stack with built in array and 
+ * Using Push and Pop Array method to insert at the end 
+ * and remove from the end Last In First Out.
+*/
+  const stack = [];
+
+stack.push('Hello')
+stack.push('World')
+stack.push('!!')
+stack.push('!!!!!')
+console.log(stack)
+console.log(stack.pop())
+console.log(stack)
