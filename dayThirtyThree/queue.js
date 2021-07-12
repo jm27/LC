59573939096accf8adf6d848a6960f52ddd247d8
Jesus Esquer <jme2791@gmail.com>
2021-07-12 08:20:28 -0700

    Queue class with js

---
+++ dayThirtyThree/queue.js
@@ -0,0 +1,54 @@
+/**
+ * Queue - First in, First Out.
+ */
+
+// Array
+// const q = [];
+
+// q.unshift(3);
+// q.shift();
+// console.log(q);
+
+// Queue class
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  // Add to the end
+  enqueue(val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+  // Remove from the beginning
+  dequeue() {
+    if (!this.first) return null;
+
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+// const qu = new Queue();
+// qu.enqueue(1);
+// qu.dequeue();
